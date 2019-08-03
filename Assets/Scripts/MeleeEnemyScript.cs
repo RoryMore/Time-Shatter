2e4ba0415c6645fe77c6941304a8863b9f2614ae
Rory <ruairidh.more@gmail.com>
2019-08-03 22:36:54 +1200

    Cleaned project
    Enemy and player use one script each, basic turn implementation in game

---
+++ Assets/Scripts/MeleeEnemyScript.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MeleeEnemyScript : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    public AudioClip deathClip;
+
+
+    Animator anim;
+    AudioSource enemyAudio;
+    ParticleSystem hitParticles;
+    CapsuleCollider capsuleCollider;
+
+    NavMeshAgent nav;
+
+    Transform playerLocation;
+    PlayerScript player;
+
+    bool isDead;
+    bool isSinking;
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        enemyAudio = GetComponent<AudioSource>();
+        hitParticles = GetComponentInChildren<ParticleSystem>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        nav = GetComponent<NavMeshAgent>();
+
+        player = GameObject.Find("Player").GetComponent<PlayerScript>();
+        playerLocation = GameObject.Find("Player").transform;
+
+        currentHealth = startingHealth;
+    }
+
+
+    void Update()
+    {
+        if (player.playerTakingAction == false)
+        {
+            nav.enabled = true;
+            Movement();
+        }
+        else
+        {
+            nav.enabled = false;
+        }
+
+
+
+    }
+
+    //Function that is called when the player deals damage to you
+    //Default condition format
+    public void TakeDamage(int amount, Vector3 hitPoint)
+    {
+        if (isDead)
+            return;
+
+        //Audio Cue
+        enemyAudio.Play();
+
+        currentHealth -= amount;
+
+        hitParticles.transform.position = hitPoint;
+        hitParticles.Play();
+
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+
+
+    void Death()
+    {
+        isDead = true;
+
+        capsuleCollider.isTrigger = true;
+
+        anim.SetTrigger("Dead");
+
+        enemyAudio.clip = deathClip;
+        enemyAudio.Play();
+    }
+
+
+
+
+    public void Movement()
+    {
+        if(currentHealth > 0 && player.currentHealth > 0)
+        {
+        nav.SetDestination(playerLocation.position);
+        }
+        else
+        {
+            nav.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public float timeLeftUntilAction = 6;
+    public bool playerTakingAction = false;
+
+
+
+    //Animator anim;
+    //AudioSource playerAudio;
+
+    bool isDead;
+    bool damaged;
+    //For animation
+    private bool running = false;
+
+    private NavMeshAgent navmeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navmeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Awake()
+    {
+        //anim = GetComponent <Animator> ();
+        //playerAudio = GetComponent <AudioSource> ();
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CheckDamage();
+        Movement();
+        CountdownToTurn();
+       
+
+
+    }
+
+    void CountdownToTurn()
+    {
+        if (playerTakingAction == false)
+        {
+            //Iterate on player timer
+            timeLeftUntilAction -= 1f * Time.deltaTime;
+            Mathf.Round(timeLeftUntilAction);
+            print(timeLeftUntilAction);
+            if (timeLeftUntilAction <= 0)
+            {
+                print("Player Turn!");
+                playerTakingAction = true;
+                TakeAction();
+                //Do player action
+            }
+        }
+
+    }
+
+    void TakeAction()
+    {
+        //Script to determine how the player actually does their turn 
+        //This one is gonna be complex!
+        navmeshAgent.enabled = false;
+        print("Were gonna pretend you did something!");
+        navmeshAgent.enabled = true;
+
+        //Once the function is  complete reset the bool and timer to enter back into movement mode
+        playerTakingAction = false;
+        timeLeftUntilAction = 6;
+
+    }
+
+    void CheckDamage()
+    {
+        //Damage related aspect
+        //Any visual damage cues here
+    }
+
+    void Movement()
+    {
+        if (playerTakingAction == false)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    navmeshAgent.destination = hit.point;
+                }
+            }
+
+            //Animation stuff
+            if (navmeshAgent.remainingDistance <= navmeshAgent.stoppingDistance)
+            {
+                running = false;
+            }
+            else
+            {
+                running = true;
+            }
+
+            //animator.SetBool("running", running);
+        }
+    }
+
+
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+
+        //healthSlider.value = currentHealth;
+
+        //playerAudio.Play ();
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        isDead = true;
+
+        
+
+        //anim.SetTrigger ("Die");
+
+        //playerAudio.clip = deathClip;
+        //playerAudio.Play ();
+
+    }
+
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
