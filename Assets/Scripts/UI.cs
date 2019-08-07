4eab4306ed96097de1bbbafdcf67033b66e210fd
Jasmine <jazzykitkat@windowslive.com>
2019-08-07 17:44:10 +1200

    healthbar

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
+    public Slider Healthbar;
+
+    PlayerScript player;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        player = FindObjectOfType<PlayerScript>();
+    }
+
     void Start()
     {
-        
+        Healthbar.value = CalculateHealth();
+        Debug.Log("YAY");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+    }
+
+    float CalculateHealth()
+    {
+        return player.currentHealth / player.maxHealth;
     }
 }
