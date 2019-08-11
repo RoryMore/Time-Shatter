4ae4e36b93cacbc34cbbfa3a1109660a04423d45
Sunny Peebles <sunny.peebles@gmail.com>
2019-08-12 11:53:45 +1200

    Basic Camera Done

--- Assets/Scripts/OurCameraController.cs
+++ Assets/Scripts/OurCameraController.cs
@@ -9,7 +9,10 @@ public class OurCameraController : MonoBehaviour
 
     public float rotateSpeed = 5.0f;
 
-    public const float yMinAngle = 10.0f;
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+
+    public const float yMinAngle = 30.0f;
     public const float yMaxAngle = 85.0f;
 
     // How far away we want our camera to be from focused point
@@ -22,16 +25,21 @@ public class OurCameraController : MonoBehaviour
     // If we want our camera to do an initial look around/movement before we start
     bool battleReady = false;
 
-    public bool invertY = false;
+    public bool invertY = true;
 
     PlayerScript playerScript = null;
 
     Vector3 cameraOffset;
 
+    float x = 0.0f;
+    float y = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        Vector3 angles = transform.eulerAngles;
+        x = angles.x;
+        y = angles.y;
     }
 
     private void Awake()
@@ -47,43 +55,45 @@ public class OurCameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        targetDistance += Mathf.Clamp(Input.mouseScrollDelta.y * 0.2f, minDistance, maxDistance);
-
-        // Hide and lock cursor when right mouse button pressed
-        if (Input.GetMouseButtonDown(1))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-
-        // Unlock and show cursor when right mouse button released
-        if (Input.GetMouseButtonUp(1))
-        {
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-        }
+        targetDistance = Mathf.Clamp(targetDistance - Input.mouseScrollDelta.y*5, minDistance, maxDistance);
 
         if (focus != null)
         {
-
-            if (Input.GetMouseButton(1))
+            // Hide and lock cursor when right mouse button pressed
+            if (Input.GetMouseButtonDown(1))
             {
-                /*float mouseYMove = Input.GetAxis("Mouse Y");
-                Quaternion horizCamTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotateSpeed, Vector3.up);
-                Quaternion vertCamTurnAngle = Quaternion.AngleAxis(mouseYMove * rotateSpeed, (invertY ? Vector3.left : Vector3.right));
-
-                cameraOffset = horizCamTurnAngle * cameraOffset;
-
-                Vector3 dir = transform.position - focus.position;
-                float angle = Vector3.Angle(transform.position, Vector3.up);
-                //Debug.Log("Angle between Camera & Player: " + angle);
-
-                cameraOffset = vertCamTurnAngle * cameraOffset;*/
+                Cursor.lockState = CursorLockMode.Locked;
             }
 
-            //Vector3 newPos = focus.position + cameraOffset;
-            //transform.position = Vector3.Slerp(transform.position, newPos, positionLerpSpeed);
+            // Unlock and show cursor when right mouse button released
+            if (Input.GetMouseButtonUp(1))
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
 
-            transform.LookAt(focus);
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+            if (Input.GetMouseButton(1))
+            {
+                x += Input.GetAxis("Mouse X") * xSpeed * Time.unscaledDeltaTime;
+                y -= Input.GetAxis("Mouse Y") * ySpeed * Time.unscaledDeltaTime * (invertY ? 1.0f : -1.0f);
+
+                
+
+                
+
+                if (Physics.Linecast(focus.position, transform.position, out RaycastHit hit))
+                {
+                    targetDistance -= hit.distance;
+                }
+
+            }
+            y = ClampAngle(y, yMinAngle, yMaxAngle);
+            Vector3 negDistance = new Vector3(0.0f, 0.0f, -targetDistance);
+            Vector3 position = rotation * negDistance + focus.position;
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 0.2f);
+            transform.position = Vector3.Slerp(transform.position, position, 0.2f);
         }
     }
 
@@ -95,5 +105,14 @@ public class OurCameraController : MonoBehaviour
     public Transform GetFocus()
     {
         return focus;
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
     }
 }
